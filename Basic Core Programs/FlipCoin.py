4642e9b4799b9ca19214c57ec7247db18a65a307
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-08 17:31:20 +0530

    [kishore]Refactor.refactor added exception

--- Basic Core Programs/FlipCoin.py
+++ Basic Core Programs/FlipCoin.py
@@ -1,15 +1,19 @@
 import random
-n = int(input("Enter the number : "))
-tail = 0
-head = 0
-for i in range(n):
-        r1 = round(random.random(),1)
-        print(r1)
-        if r1<0.5:
-            tail=tail+1
-        else:
-            head=head+1
-# print (tail,"  ",head)
-tail = (tail*100)//n
-head = (head*100)//n
-print("tails percentage : {0}\nhead percentage : {1} ".format(tail,head))
+
+try:
+	n = int(input("Enter the number : "))
+	tail = 0
+	head = 0
+	for i in range(n):
+	        r1 = round(random.random(),1)
+	        print(r1)
+	        if r1<0.5:
+	            tail=tail+1
+	        else:
+	            head=head+1
+	# print (tail,"  ",head)
+	tail = (tail*100)//n
+	head = (head*100)//n
+	print("tails percentage : {0}\nhead percentage : {1} ".format(tail,head))
+except ValueError:
+	print("Wrong input!")
