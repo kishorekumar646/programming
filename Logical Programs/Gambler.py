c0b6fc55fe1ea92cc8b0f26eba90fbdd2b3cba94
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-08 17:43:28 +0530

    [kishore]Refactor.refactor added my logic

--- Logical Programs/Gambler.py
+++ Logical Programs/Gambler.py
@@ -1,24 +1,19 @@
+import random
+
 stake = int(input("enter stake = "))
 goal = int(input("enter goal = "))
-trails = int(input("enter trails = "))
-
-bc = 1
-n=50
-og = goal
-wc = 0
-
-while(n==1):
-    if(bc == goal):
-        print("won")
-        goal = goal+og
-        wc = wc+1
-        n = n-1
-    else:
-        stake = int(input("enter stake again = "))
-        bc = stake
-   
-
-print("wc={}".format(wc))
-print("bc={}".format(bc))
-#print("per={}".format(per))
-
+n = int(input("enter trails = "))
+while(n!=0):
+    r = random.randint(0,1)
+    print(r)
+    if r==1:
+        stake+=1
+        print("stake = {}".format(stake))
+        if(stake==goal):
+            print("won")
+            break
+    elif r==0:
+        n-=1
+        print("n = {}".format(n))
+    if(n==0):
+        print("lost all chances")
