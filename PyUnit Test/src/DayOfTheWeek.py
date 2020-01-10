a7f323eeacb663a49d926a06f8c5f091dc113140
adil <adiliqbal10@gmail.com>
2020-01-10 19:08:46 +0530

    [kishore]Add.added files

--- PyUnit Test/src/DayOfTheWeek.py
+++ PyUnit Test/src/DayOfTheWeek.py
@@ -1,25 +1,31 @@
+
 def dayOfTheWeek(arr):
 	t = [0,3,2,5,0,3,5,1,4,6,2,4]
 	day,month,year = int(arr[0]),int(arr[1]),int(arr[2])
 	return (year + year//4 - year//100 + year//400 + t[month-1] + day) % 7
 
-def week(target):
-	switcher = {
-		0 : "SUNDAY",
-		1 : "MONDAY",
-		2 : "TUESDAY",
-		3 : "WEDNESDAY",
-		4 : "THURSDAY",
-		5 : "FRIDAY",
-		6 : "SATURDAY"
-	}
-	return switcher.get(target,"nothing")
+def func():
+	return True
 
-try:
-	date = str(input("Enter the date DD-MM-YYYY : ")).split("-")
-	if len(date) <= 1:
-		raise ValueError
-	d = dayOfTheWeek(date)
-	print("\nThe day is : ",week(d),"\n")
-except ValueError:
-	print("\nThe date format should be like this DD-MM-YYYY\n")
+# 	def week(self,target):
+# 		switcher = {
+# 			0 : "SUNDAY",
+# 			1 : "MONDAY",
+# 			2 : "TUESDAY",
+# 			3 : "WEDNESDAY",
+# 			4 : "THURSDAY",
+# 			5 : "FRIDAY",
+# 			6 : "SATURDAY"
+# 		}
+# 		return switcher.get(target,"nothing")
+
+# if __name__ == "__main__":
+# 	try:
+# 		date = str(input("Enter the date DD-MM-YYYY : ")).split("-")
+# 		if len(date) <= 1:
+# 			raise ValueError
+# 		d = Day()
+# 		f=d.dayOfTheWeek(date)
+# 		print("\nThe day is : ",d.week(f),"\n")
+# 	except ValueError:
+# 		print("\nThe date format should be like this DD-MM-YYYY\n")
---
+++ PyUnit Test/tests/test_cases.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.append("/home/user/Desktop/PythonPrograms/programming/PyUnit Test")
+from src.DayOfTheWeek import dayOfTheWeek,func
+
+lst = [15,3,1998]
+print(func())
+print(dayOfTheWeek(lst))
+
