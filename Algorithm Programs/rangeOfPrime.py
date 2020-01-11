72f0c71e912b271c09ae072d51f7d64b18095668
adil <adiliqbal10@gmail.com>
2020-01-11 12:15:44 +0530

    [kishore]Fix.fix error find and fix it

--- Algorithm Programs/rangeOfPrime.py
+++ Algorithm Programs/rangeOfPrime.py
@@ -1,9 +1,24 @@
+
+
+def Anagrams(arr):
+    anagram_list = []
+    for i in arr:
+        for j in arr:
+            if (i != j) and (sorted(str(i)) == sorted(str(j))):
+                anagram_list.append(i)
+    print(anagram_list)
+
+
+
 start = int(input("Enter range number\nstart : "))
 end = int(input("end : "))
+lst = []
 for i in range(start,end+1):
     if i > 1:
         for j in range(2,i):
             if (i % j) == 0:
                 break
         else:
-            print(i)
+            lst.append(i)
+print(lst)
+Anagrams(lst)
