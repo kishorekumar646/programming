034955f5ad93f5a313d60d864617120524123a25
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-17 19:08:05 +0530

    [kishore]Refactor.refactor writing discription of program

--- FunctionalPrograms/Distance.py
+++ FunctionalPrograms/Distance.py
@@ -1,11 +1,16 @@
 import math
 
+"""    formula to calculate distance = sqrt(x*x + y*y)     """
+
 def calculateDistance(a,b):
 	return round(math.sqrt(a**2 + b**2),3) 
 
 try:
+
 	x = int(input("value of x : "))
 	y = int(input("value of y : "))
 	print (calculateDistance(x,y))
+
 except ValueError:
+	
 	print("\nEnter Integer only ! \n")
