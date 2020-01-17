9d79e191a536ccb62323f7ea44dab2dbbde01d58
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-17 18:57:02 +0530

    [kishore]Refactor.refactor adding commenting of methods and change variables names

--- FunctionalPrograms/Array_2D.py
+++ FunctionalPrograms/Array_2D.py
@@ -1,14 +1,25 @@
-def displaying_2D_Array(array,rows,col):
-	for i in range(rows):
-		for j in range(col):
-			print(array[i][j],end=" ")
+"""
+	A library for reading in 2D arrays of integers input and printing them out to standard output.
+
+"""
+
+def displaying_2D_Array(array,rows_size,column_size):
+	
+	for row in range(rows_size):
+		for column in range(column_size):
+			print(array[row][column],end=" ")
 		print()
 	print()
 
 try:
-	rows_size = int(input("Enter the rows size : "))
-	col_size  = int(input("Enter the col size : "))
-	lst = [[int(input()) for i in range(rows_size)] for j in range(col_size)]  #here taking the 2D array elements
-	displaying_2D_Array(lst,rows_size,col_size)
+
+	input_rows_size = int(input("Enter the rows size : "))
+	input_column_size  = int(input("Enter the col size : "))
+
+	#here taking input the 2D array elements
+	array_list = [[int(input()) for row_number in range(input_rows_size)] for column_number in range(input_column_size)]
+	displaying_2D_Array(array_list,input_rows_size,input_column_size)
+
 except ValueError:
+
 	print("Enter only Integers")
