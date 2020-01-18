6efc558c6ec9a1dc549fdcdc60a10a3fa6f6039a
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-18 14:56:04 +0530

    [kishore]Refactor.refactor adding the exception and naming convention in program

--- LogicalPrograms/Gambler.py
+++ LogicalPrograms/Gambler.py
@@ -1,19 +1,34 @@
 import random
 
-stake = int(input("enter stake = "))
-goal = int(input("enter goal = "))
-n = int(input("enter trails = "))
-while(n!=0):
-    r = random.randint(0,1)  #here generate random integers 
-    print(r)
-    if r==1:
-        stake+=1
-        print("stake = {}".format(stake))
-        if(stake==goal):
-            print("won")
-            break
-    elif r==0:
-        n-=1
-        print("n = {}".format(n))
-    if(n==0):
-        print("lost all chances")
+"""
+    Simulates a gambler who start with $stake and place fair $1 bets until he/she goes broke (i.e. has no money) or 
+    reach $goal. Keeps track of the number of times he/she wins and the number of bets he/she makes. 
+    Run the experiment N times, averages the results, and prints them out.
+"""
+try:
+
+    stake = int(input("enter stake = "))
+    goal = int(input("enter goal = "))
+    trails = int(input("enter trails = "))
+
+    while(trails!=0):
+
+        random_number = random.randint(0,1)
+        print(random_number)
+
+        if random_number==1:
+            stake+=1
+            print("stake = {}".format(stake))
+            if(stake==goal):
+                print("won")
+                break
+
+        elif random_number==0:
+            trails-=1
+            print("trails = {}".format(trails))
+
+        if(trails==0):
+            print("lost all chances")
+
+except ValueError:
+    print("Error")
