bba168c7eff56e5def12b01fae24d244f8d1e740
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-18 12:41:25 +0530

    [kishore]Refactor.refactor changed the variables names

--- AlgorithmPrograms/insertionSort.py
+++ AlgorithmPrograms/insertionSort.py
@@ -1,14 +1,32 @@
-def insertionSort(arr):
-    for i in range(1,len(arr)):
-        key = arr[i]
-        j = i-1
-        while arr[j] > key and j>=0:
-            arr[j+1] = arr[j]
-            j = j - 1
-        arr[j+1] = key
-    print(arr)
+"""
 
-n = int(input("Enter the size of array : "))
-lst = [int(input()) for i in range(n)]
-print(lst)
-insertionSort(lst)
+    Reads in inegers and prints them in sorted order using insertion sort.
+
+"""
+
+def insertionSort(array_list):
+    
+    for index in range(1, len(array_list)):
+        
+        key = array_list[index]
+        position = index-1
+        
+        while (position >= 0 and array_list[position] > key):
+        
+            array_list[position+1] = array_list[position]
+            position = position-1
+        
+        array_list[position+1] = key
+    
+    print(array_list)
+
+try:
+
+    list_size = int(input("Enter the size of array : "))
+    data_list = [int(input()) for take_list in range(list_size)]
+    print(data_list)
+    insertionSort(data_list)
+
+except ValueError:
+
+    print("Take input integers only !")
