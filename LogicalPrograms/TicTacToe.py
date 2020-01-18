7cf0dcf2e5d314638ec1551744b48dee434fb7fe
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-18 15:56:05 +0530

    [kishore]Refactor.refactor add the exception in program

--- LogicalPrograms/TicTacToe.py
+++ LogicalPrograms/TicTacToe.py
@@ -5,38 +5,49 @@ class TicTacToe:
     player = X
     isEmpty = True
 
+
     #inserting the board positions
     def insert(self,x,y):   
+        
         if (x<0) or (x>2) or (y<0) or (y>2):
             print("Invalid Board Position")
             return
+        
         if self.board[x][y] != self.EMPTY:
             print("Board position occupied")
             return
+        
         self.board[x][y] = self.player
         self.player = -self.player
 
+    
     #Displaying the board
     def displayBoard(self):
+        
         self.isEmpty=False
         print(self.board,"\n")
-        for i in range(3):
-            for j in range(3):
-                if self.board[i][j] == 1:
+
+        for row in range(3):
+            for column in range(3):
+                if self.board[row][column] == 1:
                     print(" X ",end="")
-                if self.board[i][j] == -1:
+                if self.board[row][column] == -1:
                     print(" O ",end="")
-                if self.board[i][j] == self.EMPTY:
+                if self.board[row][column] == self.EMPTY:
                     print("   ",end="")
                     self.isEmpty=True
-                if j<2:
+                if column<2:
                     print("|",end="")
-            if i<2:
+            
+            if row<2:
                 print("\n-----------")
+        
         print("\n")
+
 
     #checking the winner
     def isWin(self,player):
+
         return (
             (self.board[0][0] + self.board[0][1] + self.board[0][2] == player*3) or
             (self.board[1][0] + self.board[1][1] + self.board[1][2] == player*3) or
@@ -48,26 +59,43 @@ class TicTacToe:
             (self.board[2][0] + self.board[1][1] + self.board[0][2] == player*3) 
         )
 
+
     #display the winner
     def displayWinner(self):
+        
         if self.isWin(self.X):
             print("\n  X WINNER  \n")
             self.isEmpty=False
+        
         elif self.isWin(self.O):
             print("\n  O WINNER  \n")
             self.isEmpty=False
+        
         else:
             if (self.isEmpty == False):
                 print ("\n  XO DRAW  \n")
 
-# here start the game                
-t = TicTacToe()
-while t.isEmpty:
-    print("Player X turn" if t.player == t.X else "Player O turn")
-    print("Enter x and y places : ")
-    x = int(input("x : "))
-    y = int(input("y : "))
-    t.insert(x,y)
-    t.displayBoard()
-    print("_________________________\n")
-    t.displayWinner()
+
+try:
+    
+    # here start the game                
+    t = TicTacToe()
+    while t.isEmpty:
+        
+        print("Player X turn" if t.player == t.X else "Player O turn")
+        
+        print("Enter x and y places : ")
+        x_axis = int(input("x : "))
+        y_axis = int(input("y : "))
+        
+        t.insert(x_axis,y_axis)
+        
+        t.displayBoard()
+        
+        print("_________________________\n")
+        
+        t.displayWinner()
+
+except ValueError:
+    
+    print("Input wrong")
