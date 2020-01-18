d0237421a83f5da21a0b522539b88ad691b4148d
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-18 11:32:31 +0530

    [kishore]Refactor.refactor following the indentation and naming convention of program

--- AlgorithmPrograms/BubbleSort.py
+++ AlgorithmPrograms/BubbleSort.py
@@ -1,11 +1,29 @@
-def BubbleSort(arr):
-    for i in range(len(arr)-1):
-        for j in range(len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j],arr[j+1] = arr[j+1],arr[j]
-    print(arr)
+"""
+   
+    Reads in integers prints them in sorted order using Bubble Sort
 
-n = int(input("Enter the size of array : "))
-lst = [int(input()) for i in range(n)]
-print(lst)
-BubbleSort(lst)
+"""
+
+def BubbleSort(array_list):
+
+    for index1 in range(len(array_list)-1):
+        for index2 in range(len(array_list)-index1-1):
+            if array_list[index2] > array_list[index2+1]:
+                array_list[index2],array_list[index2+1] = array_list[index2+1],array_list[index2]
+    
+    print(array_list)
+
+try:
+
+    list_size = int(input("Enter the size of array : "))
+    
+    data_list = [int(input()) for take_list in range(list_size)]
+    
+    print(data_list)
+    
+    #Method calling
+    BubbleSort(data_list)
+
+except ValueError:
+    
+    print("Input should be an Integer")
