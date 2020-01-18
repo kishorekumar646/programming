4f84f9e4a88e07e796eacadd27a93446aad130fe
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-18 10:43:43 +0530

    [kishore]Refactor.refactor add indentation

--- BasicCorePrograms/PrimeFactors.py
+++ BasicCorePrograms/PrimeFactors.py
@@ -7,20 +7,26 @@ import math
 
 
 def printing_primeFactorsNumbers(num):
+
     print("Prime Factors are : ",end="")
     while (num%2) == 0:
         print(2,end=" ")
         num //=2
+    
     for check_prime_number in range(3,int(math.sqrt(num))+1,2):
         while (num%check_prime_number) == 0:
             print(check_prime_number,end=" ")
             num //=check_prime_number
+    
     if num>2:
         print(num,end="")
 
 try:
+
     taking_input = int(input("Enter the number : "))
     printing_primeFactorsNumbers(taking_input)
     print()
+
 except ValueError:
+ 
     print("Input should be an Integer ! ")
