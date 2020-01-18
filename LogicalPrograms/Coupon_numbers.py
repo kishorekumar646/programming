62f729c9f27a13a6073a0468d2f3cfbb3d254385
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-18 15:08:30 +0530

    [kishore]Refactor.refactor changed the variables names

--- LogicalPrograms/Coupon_numbers.py
+++ LogicalPrograms/Coupon_numbers.py
@@ -1,20 +1,32 @@
 import random
+"""
+    Given N distinct Coupon Numbers, how many random numbers do you need to generate distinct coupon number? 
+    This program simulates this random process.
+"""
 
-def print_distinct_couponNumbers(num,c,m,a):
-    while (m!=num):
-        x = random.randint(0,num)
-        c+=1
-        if x not in a:
-            a.append(x)
-            m+=1
-    print("Distinct Coupon List : ",a)
-    print("Iteration for to print the Coupon numbers : ",c)
+def print_distinct_couponNumbers(number_count,random_count,distinct_coupon_count,list_coupon):
+    
+    while (distinct_coupon_count!=number_count):
+        
+        x = random.randint(0,number_count)
+        random_count += 1
+        
+        if x not in list_coupon:
+            list_coupon.append(x)
+            distinct_coupon_count+=1
+    
+    print("Distinct Coupon List : ",list_coupon)
+    print("Iteration for to print the Coupon numbers : ",random_count)
+
 
 try:
+
     couponList = []
     iteration_count = 0
     couponNumbersCount = 0
     distinct_couponNumber_Count = int(input("enter num = "))
     print_distinct_couponNumbers(distinct_couponNumber_Count,iteration_count,couponNumbersCount,couponList)
+
 except ValueError:
+
     print("Enter Integers only ! ")
