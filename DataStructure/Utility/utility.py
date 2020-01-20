131da2d8709ef51b95f10b7e7a25e1b3372660de
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-20 10:23:47 +0530

    [kishore]Refactor.refactor added methods for linked list

--- DataStructure/Utility/utility.py
+++ DataStructure/Utility/utility.py
@@ -29,22 +29,63 @@ class LinkedList:
            
             last.next = node
     
+    def remove(self,item):
+        pass
+
+    def search(self,item):
+        pass
+
+    def isEmpty(self):
+
+        if self.head == None:
+            return True
+
+        else:
+            return False
+
+    def size(self):
+        
+        return self.sizeList
+
+    def append(self,item):
+        pass
+
+    def index(self,item):
+        pass
+
+    def insert(self,pos,item):
+        pass
+
+    def pop(self):
+
+        if self.head == None:
+            return None
+
+        if self.head.next == None:
+            self.head = None
+            return None
+
+        current = self.head
+        while current.next.next:
+            current = current.next
+
+        current.next = None
+
+    # def pop(self,pos):
+    #     pass
+
     def insertFirst(self,ele):
         
         node = Node(ele)
         node.next = self.head
         self.head = node
 
-    def size(self):
-        
-        return self.sizeList
-
     def dispalyList(self):
         
         current = self.head
         
         while current:
-            print(current.data," ----> ",end="")
+            print(current.data," <---- ",end="")
             current = current.next
         
         print("None")
