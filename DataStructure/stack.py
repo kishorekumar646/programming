a7150ebfacc56ebb1d151cf59b2c19d273ceae6e
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-20 19:30:01 +0530

    [kishore]Refactor.refactor impliment stack

--- DataStructure/stack.py
+++ DataStructure/stack.py
@@ -1,2 +1,33 @@
 from Utility import utility
 
+try:
+
+    # input_parenthesis = str(input("Enter parenthesis expression : "))
+
+    stack = utility.Stack()
+
+    print("1.Insert\t2.Display List\t3.Top Element\t4.Delete\t5.Terminate")
+    b = True
+    while b:
+        choice = int(input("Enter your choice : "))
+        if choice == 1:
+            item = int(input("Enter the number : "))
+            stack.push(item)
+        
+        elif choice == 2:
+            stack.dispaly_stack_list()
+        
+        elif choice == 3:
+            print("Top element is : ",stack.peek())
+
+        elif choice == 4:
+            print("Delete element is : ",stack.pop())
+
+        elif choice == 5:
+            b = False
+            print("  --------  Close Stack  -------")
+        else:
+            print("Wrong input")
+except ValueError:
+
+    print("Error")
