6298dd7a156fb3d1a4deead69ec83fb1c799f8f9
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-21 11:18:16 +0530

    [kishore]Refactor.refactor implement balance paranthases

--- DataStructure/stack.py
+++ DataStructure/stack.py
@@ -2,32 +2,36 @@ from Utility import utility
 
 try:
 
-    # input_parenthesis = str(input("Enter parenthesis expression : "))
-
     stack = utility.Stack()
-
-    print("1.Insert\t2.Display List\t3.Top Element\t4.Delete\t5.Terminate")
-    b = True
-    while b:
-        choice = int(input("Enter your choice : "))
-        if choice == 1:
-            item = int(input("Enter the number : "))
-            stack.push(item)
-        
-        elif choice == 2:
+    input_parenthesis = str(input("Enter parenthesis expression : "))
+    flag = 0
+    for token in input_parenthesis:
+        print("Token : ",token)
+        if (token == '{') or (token == '(') or (token == '['):
+            stack.push(token)
             stack.dispaly_stack_list()
         
-        elif choice == 3:
-            print("Top element is : ",stack.peek())
-
-        elif choice == 4:
-            print("Delete element is : ",stack.pop())
-
-        elif choice == 5:
-            b = False
-            print("  --------  Close Stack  -------")
         else:
-            print("Wrong input")
+            top_list = stack.pop()
+            stack.dispaly_stack_list()
+            print("Top : ",top_list)
+            if top_list == '(':
+                if (token == '}') or (token ==']'):
+                    flag = 1
+            if top_list == '[':
+                if (token == '}') or (token ==')'):
+                    flag = 1
+            if top_list == '{':
+                if (token == ')') or (token ==']'):
+                    flag = 1
+        # stack.dispaly_stack_list()
+        
+    if flag == 0:
+        print("Balanced")
+    else:
+        print("Not Balanced")
+    
+
 except ValueError:
 
     print("Error")
