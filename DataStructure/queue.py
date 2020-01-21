1f9bdfbe44d0673e673449987e4047a9eb6a4ff4
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-21 15:46:58 +0530

    [kishore]Refactor.refactor add methods

--- DataStructure/queue.py
+++ DataStructure/queue.py
@@ -1,23 +1,52 @@
 from Utility import utility
 
+class Banking_cash_counter:
+
+    def __init__(self):
+        
+        self.bank_balance = 10000
+
+    def deposite_amount(self,money):
+        
+        self.bank_balance += money
+
+    def withdraw_amount(self,money):
+        
+        self.bank_balance -= money
+
+    def bank_balance_amount(self):
+        
+        return self.bank_balance
+
 try:
 
-    queue = utility.Queue()
-    queue.enqueue(10)
-    queue.enqueue(20)
-    queue.enqueue(30)
+    bank = Banking_cash_counter()
+    
+    print("1.Deposit\t2.Withdraw\t3.Bank Balance\t4.terminate")
+    
+    b = True
+    
+    while b:
+        
+        choice = int(input("Enter your choice : "))
 
-    queue.dispaly_queue_list()
+        if choice == 1:
+            amount = int(input("Enter amount to deposite : "))
+            bank.deposite_amount(amount)
 
-    queue.dequeue()
-    queue.dispaly_queue_list()
-    queue.dequeue()
-    queue.dispaly_queue_list()
-    queue.dequeue()
-    queue.dispaly_queue_list()
-    queue.dequeue()
-    queue.enqueue(10)
-    queue.dispaly_queue_list()
+        elif choice == 2:
+            amount = int(input("Enter amount to withdraw : "))
+            bank.withdraw_amount(amount)
+
+        elif choice == 3:
+            print("Bank balance : ",bank.bank_balance_amount())
+
+        elif choice == 4:
+            b = False
+
+        else:
+            print("Wrong input please try again")
 
 except ValueError:
+
     print("Error")
