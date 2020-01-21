ba380e40c387288643916fbedac00b800b233fd3
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-21 14:26:24 +0530

    [kishore]Refactore.refactor stack method

--- DataStructure/stack.py
+++ DataStructure/stack.py
@@ -2,9 +2,11 @@ from Utility import utility
 
 def check_balanced_parenthesis(parenthesis_string):
 
+    stack = utility.Stack()
+
     for token in parenthesis_string:
 
-        if (token == '{') or (token == '(') or (token == '['):
+        if (token == '(') or (token == '{') or (token == '['):
             stack.push(token)
         
         else:
@@ -22,14 +24,13 @@ def check_balanced_parenthesis(parenthesis_string):
                 if (token == ')') or (token ==']'):
                     return False
 
-    if stack.isEmpty == True:
+    if stack.isEmpty:
         return True
     else:
         return False
 
 try:
 
-    stack = utility.Stack()
     input_parenthesis = str(input("Enter parenthesis expression : "))
     if check_balanced_parenthesis(input_parenthesis):
         print(input_parenthesis," is Balanced")
