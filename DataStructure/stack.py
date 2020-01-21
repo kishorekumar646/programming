5f552e7549c5e373d946ca8d3edcd666c4b5b2a8
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-21 11:36:23 +0530

    [kishore]Refactor.refactor reduce the code

--- DataStructure/stack.py
+++ DataStructure/stack.py
@@ -1,37 +1,43 @@
 from Utility import utility
 
+def check_balanced_parenthesis(parenthesis_string):
+
+    for token in parenthesis_string:
+
+        if (token == '{') or (token == '(') or (token == '['):
+            stack.push(token)
+        
+        else:
+            top_list = stack.pop()
+
+            if top_list == '(':
+                if (token == '}') or (token ==']'):
+                    return False
+            
+            if top_list == '[':
+                if (token == '}') or (token ==')'):
+                    return False
+            
+            if top_list == '{':
+                if (token == ')') or (token ==']'):
+                    return False
+
+    if stack.isEmpty == True:
+        return True
+    else:
+        return False
+
 try:
 
     stack = utility.Stack()
     input_parenthesis = str(input("Enter parenthesis expression : "))
-    flag = 0
-    for token in input_parenthesis:
-        print("Token : ",token)
-        if (token == '{') or (token == '(') or (token == '['):
-            stack.push(token)
-            stack.dispaly_stack_list()
-        
-        else:
-            top_list = stack.pop()
-            stack.dispaly_stack_list()
-            print("Top : ",top_list)
-            if top_list == '(':
-                if (token == '}') or (token ==']'):
-                    flag = 1
-            if top_list == '[':
-                if (token == '}') or (token ==')'):
-                    flag = 1
-            if top_list == '{':
-                if (token == ')') or (token ==']'):
-                    flag = 1
-        # stack.dispaly_stack_list()
-        
-    if flag == 0:
-        print("Balanced")
-    else:
-        print("Not Balanced")
+    if check_balanced_parenthesis(input_parenthesis):
+        print(input_parenthesis," is Balanced")
     
+    else:
+        print(input_parenthesis," is Not Balanced")
 
+    
 except ValueError:
 
-    print("Error")
+    print("Wrong input")
