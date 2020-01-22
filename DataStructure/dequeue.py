816da03d72a2adbb05a1a4ac0af7c6e06bd216cc
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-22 19:25:24 +0530

    [kishore]Refactor.refactor implement dequeue

--- DataStructure/dequeue.py
+++ DataStructure/dequeue.py
@@ -2,13 +2,13 @@ from Utility import utility
 
 
 try:
+    
     dequeue = utility.Dequeue()
-
-    dequeue.addFront(10)
-    dequeue.addFront(20)
-    dequeue.addRear(30)
-    dequeue.dispaly_dequeue_list()
-    dequeue.removeRear()
+    input_string = str(input("Enter the string : "))
+    for character in input_string:
+        dequeue.addFront(character)
+    print(dequeue.removeFront())
+    print(dequeue.size())
     dequeue.dispaly_dequeue_list()
 
 
