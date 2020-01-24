79637458856f1a16f1958f2a2eebe5e5bc183192
narasimhar123 <narasimhaofficial3@gmail.com>
2020-01-24 23:05:55 +0530

    [kishore]Refactor.refactor file_system.py

--- OOPS/Address_Book/file_system.py
+++ OOPS/Address_Book/file_system.py
@@ -1,11 +1,23 @@
 import json
+from person import Person
+from address_book import AddressBook
+
+
 class FileSystem:
 
-    def readFile(self,file_name):
-        with open(file_name,'r') as file_list:
+    def readFile(self, file_name):
+        persons = []
+        count = 0
+        with open(file_name, 'r') as file_list:
             s = json.load(file_list)
-            return s
+            for person_dict in s['persons']:
+                persons.append(Person(person_dict))
+                count +=1
 
-    def saveFile(self,data,file_name):
-        with open(file_name,'w') as file_list:
-            json.dump(data,file_list,indent=4)
+            addressbook = AddressBook(persons, count, s['file_name'])
+
+        return addressbook
+
+    def saveFile(self, file_name, address_book_object):
+        with open(file_name, 'w') as file_list:
+            json.dump(address_book_object.to_dict(), file_list, indent=4)
