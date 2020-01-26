c39d9d03fbf7d7e8e0212329b4710860752db31e
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-26 23:35:49 +0530

    [kishore]Refactor.refactor methods

--- OOPS/Clinic_management/doctor_manager.py
+++ OOPS/Clinic_management/doctor_manager.py
@@ -8,11 +8,20 @@ class Doctors:
         self.__avaliability = dict_doctors['avaliability']
         self.__appointment_avaliable = dict_doctors['appointment_avaliable']
 
-    def add_doctor(self,doctor_dict):
-        pass
+    def get_name(self):
+        return self.__name
 
-    def print_doctors_list(self):
-        pass
+    def get_id_number(self):
+        return self.__id_number
+
+    def get_specialization(self):
+        return self.__specialization
+    
+    def get_avaliability(self):
+        return self.__avaliability(self)
+
+    def get_appointment_avaliable(self):
+        return self.__appointment_avaliable
 
     def to_dictionary(self):
         return {
