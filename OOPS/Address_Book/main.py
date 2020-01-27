8084e48c20dc507140d4cd520d76a6a040f5589f
kishorekumar646 <kishorekumar131646@gmail.com>
2020-01-27 17:45:36 +0530

    [kishore]Refactor.refactor add commenting of main.py

--- OOPS/Address_Book/main.py
+++ OOPS/Address_Book/main.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     try:
 
         address_book_controll_object = AddressBookController()
-        b = True
-        while b:
+        is_running = True
+        while is_running:
 
             print("\n1.Open address book\t2.Create adress book\t3.Quit application\n")
             choice = int(input("Enter your choice : "))
@@ -20,7 +20,8 @@ if __name__ == "__main__":
                 address_book_controll_object.create_address_book()
 
             elif choice == 3:
-                b = address_book_controll_object.quit_application()
+                # b = address_book_controll_object.quit_application()
+                is_running = False
                 print("\n\t---------- stored data  ----------\n")
 
             else:
